b2ff8678228f94eda9ea67e73025ebdaf3ac6aac
EdmondLee958 <edmondlee958@gmail.com>
2023-11-20 16:31:49 -0800

    added components folder and converted arrowIcon to svelte

---
+++ myapp/components/ArrowIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let className = '';
+    export let color = 'text-primary'; 
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    class={className}
+>
+    <path
+        class="stroke-current {color}"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14M12 19l7-7-7-7"
+    ></path>
+</svg>
