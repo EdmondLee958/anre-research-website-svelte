5b42ede97e8aac078c1c131381a98a4415e3c3a3
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2023-11-21 19:54:47 -0800

    added customLink and Footer

---
+++ myapp/components/CustomLink.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let href = "/";
+    export let otherProps = "";
+</script>
+
+<nav>
+    <a href={href}>{otherProps}</a>
+</nav>
+<!-- import Link from 'next/link';
+
+export default function CustomLink({ as, href, ...otherProps }) {
+  return (
+    <>
+      <Link as={as} href={href}>
+        <a {...otherProps} />
+      </Link>
+    </>
+  );
+} -->
