fcb43a7e775f7d6bed65a16fa9d31f649f8c62bb
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2023-11-23 00:10:33 -0800

    fixed some bugs in circularMask and customLink and Header

--- myapp/components/CustomLink.svelte
+++ myapp/components/CustomLink.svelte
@@ -7,14 +7,3 @@
   <slot>
   </slot>
 </a>
-<!-- import Link from 'next/link';
-
-export default function CustomLink({ as, href, ...otherProps }) {
-  return (
-    <>
-      <Link as={as} href={href}>
-        <a {...otherProps} />
-      </Link>
-    </>
-  );
-} -->
