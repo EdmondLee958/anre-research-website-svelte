1149276769c9f7797c27093c85e563434771418c
EdmondLee958 <edmondlee958@gmail.com>
2023-11-23 14:11:54 -0800

    Added Layout component excluding the theme change and gradient background

---
+++ myapp/components/Layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+    import './Layout.module.css'; 
+
+    onMount(() => {
+        setAppTheme();
+    });
+
+    function setAppTheme() {
+        document.documentElement.classList.add('dark');
+    }
+</script>
+
+<div class="layout">
+    <slot></slot> 
+</div>
